f2f44e3e4528fa23ec93ecb4915abf36e918bafd
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-06-11 15:09:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./styles/index.css";
+
+/* 
+Your store item should have the following structure
+
+{
+  id: "001-beetroot", <- the item id matches the icon name in the assets/icons folder
+  name: "beetroot",
+  price: 0.35 <- You can come up with your own prices
+}
+
+*/
+
+export default function App() {
+  return <div className="App">This is where React happens li</div>;
+}
